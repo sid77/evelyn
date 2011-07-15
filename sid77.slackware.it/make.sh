9ef07ae493b1292659a0251f21cf34f533600fa1
Marco Bonetti <sid77@slackware.it>
2011-07-15 15:35:20 +0200

    oops

--- sid77.slackware.it/make.sh
+++ sid77.slackware.it/make.sh
@@ -1,4 +1,4 @@
 pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/sid77.slackware.it.list /etc/apt/sources.list.d
 pkg: mkdir -p /etc/apt/trusted.gpg.d
-pkg: cp -a %/sid77.slackware.it.gpg /etc/apt/sources.list.d
+pkg: cp -a %/sid77.slackware.it.gpg /etc/apt/trusted.list.d
