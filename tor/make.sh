aea78cdeb79df01d001b28512fd3fa03fedba3ab
Marco Bonetti <sid77@slackware.it>
2012-05-25 12:37:34 +0200

    more flags, --with-tor-user and --with-tor-group will probably fails, though

--- tor/make.sh
+++ tor/make.sh
@@ -1,4 +1,4 @@
 pkg:setup
-CFLAGS="-DPRIVATE -DNO_APPLE_EXTENSIONS" pkg:configure --sysconfdir=/etc
+CFLAGS="-DPRIVATE -DNO_APPLE_EXTENSIONS" pkg:configure --sysconfdir=/etc --localstatedir=/var --with-tor-user=_tor --with-tor-group=_tor
 make
 pkg:install
