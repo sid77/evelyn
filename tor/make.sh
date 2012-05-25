0947bfc860d0deb38df448a1bf0cb8d5b9b6a718
Marco Bonetti <sid77@slackware.it>
2012-05-25 12:40:01 +0200

    /var/lib/tor

--- tor/make.sh
+++ tor/make.sh
@@ -1,4 +1,5 @@
 pkg:setup
 CFLAGS="-DPRIVATE -DNO_APPLE_EXTENSIONS" pkg:configure --sysconfdir=/etc --localstatedir=/var --with-tor-user=_tor --with-tor-group=_tor
 make
+pkg: mkdir -p /var/lib/tor
 pkg:install
